c0e8a395b1badd924921e3d823c04a3437e7f322
Eduardo <jetomaz@gmail.com>
2020-10-16 16:04:36 -0300

    Carga

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'meu-primeiroComponent',
+    template : `
+        <p> Meu Primeiro Component Com Angular 2+ </p>
+    `
+})
+
+export class MeuPrimeiroComponent{
+
+}
